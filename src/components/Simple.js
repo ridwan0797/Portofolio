2f217a5ec9e5976f14ae8b9b0d8a8e5b069f2769
ridwan0797 <ridwan0797@gmail.com>
2022-12-30 16:52:23 +0700

    grid to flex

--- src/components/Simple.js
+++ src/components/Simple.js
@@ -10,11 +10,11 @@ function App() {
   })
 
   return (
-    <div className='w-screen h-screen bg-white'>
-      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 h-full'>
-        <div className='bg-white col-span-1 h-full'>
-          <div className='m-2 h-full'>
-            <div className='flex flex-col h-full gap-2'>
+    <div className='w-screen h-screen bg-white overflow-x-hidden md:overflow-x-hidden lg:overflow-x-hidden overflow-y-visible lg:overflow-y-hidden'>
+      <div className='flex flex-col md:flex-col lg:flex-row gap-2 h-auto lg:h-full'>
+        <div className='flex-none w-full md:w-full lg:w-3/12 h-auto md:h-full lg:h-full'>
+          <div className='m-2 h-full w-full'>
+            <div className='flex flex-col gap-2' style={{height: '80%'}}>
               <div className='flex-none h-1/2 m-7 text-black text-justify font-normal'>
                 <div className='text-sm'>Ridwan Maulana</div>
 
@@ -40,7 +40,7 @@ function App() {
 
               </div>
 
-              <div className='flex-none h-8 mt-12 mx-10'>
+              <div className='flex-none h-8 mt-0 lg:mt-12 mx-10'>
                 <div className='border-t border-gray-200 h-8'></div>
               </div>
 
@@ -59,7 +59,7 @@ function App() {
 
               </div>
 
-              <div className='flex-grow m-4 mt-5 lg:mt-28 w-full h-full'>
+              <div className=' none flex-grow m-4 mt-0 lg:mt-20 w-full h-full'>
                 <button className='bg-white hover:bg-gray-100 border rounded-md  text-black text-xs outline px-12 md:px-12 lg:px-36 py-2'> SAY HELLO! </button>
               </div>
             </div>
@@ -67,8 +67,8 @@ function App() {
 
         </div>
 
-        <div className='col-span-2 md:col-span-2 lg:col-span-2 h-96 md:h-full lg:h-full overflow-x-hidden text-center' >
-          <div className='mx-2 my-6 flex flex-row justify-center h-3/3'>
+        <div className='flex-grow md:flex-grow lg:col-span-2 h-auto md:h-full lg:h-full overflow-x-hidden text-center' >
+          <div className='mx-2 my-6 flex flex-row justify-center'>
             <img alt='images'  width={'90%'} className='object-fill' src='https://uploads-ssl.webflow.com/63640793650a2e24af80903e/63709a72b73c8d0ddd709131_COMP_iMac-p-1600.jpg' style={{height: 'auto'}} />  
           </div>
 
